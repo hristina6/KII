169a830b7422ae60505a443a5a0c9b75b3564c53
hristina6 <trajceskahristina@gmail.com>
2024-06-27 02:04:17 +0200

    Initial commit

---
+++ src/main/java/com/example/wplabs2/entity/AuthorFullNameConverter.java
@@ -0,0 +1,23 @@
+package com.example.wplabs2.entity;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class AuthorFullNameConverter implements AttributeConverter<AuthorFullName, String> {
+
+
+    @Override
+    public String convertToDatabaseColumn(AuthorFullName authorFullName) {
+        return authorFullName.getName() + " " + authorFullName.getSurname();
+    }
+
+    // String (Mladen Jovanovski) -> AuthorFullName(Mladen, Jovanovski)
+    @Override
+    public AuthorFullName convertToEntityAttribute(String s) {
+        String name = s.split("\\s+")[0];
+        String surname = s.split("\\s+")[1];
+        return new AuthorFullName(name, surname);
+    }
+}
+
